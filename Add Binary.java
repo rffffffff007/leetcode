af160ced9f4bbec922461dd0e9d2176ebdbc0404
rffffffff007 <rffffffff007@yahoo.com.cn>
2013-01-30 13:13:14 +0800

    add 3 items

---
+++ Add Binary.java
@@ -0,0 +1,52 @@
+public class Solution {
+	public String addBinary(String a, String b) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		char[] ca = a.toCharArray();
+		char[] cb = b.toCharArray();
+		int lena = ca.length;
+		int lenb = cb.length;
+		reverse(ca);
+		reverse(cb);
+		offset(ca, -'0');
+		offset(cb, -'0');
+		int lenc = Math.max(lena, lenb) + 1;
+		char[] cc = new char[lenc];
+		Arrays.fill(cc, '\0');
+		add(cc, ca);
+		add(cc, cb);
+		offset(cc, '0');
+		reverse(cc);
+		int index = 0;
+		while (index < cc.length - 1 && cc[index] == '0') {
+			index++;
+		}
+		return new String(cc, index, cc.length - index);
+	}
+
+	private void add(char[] ca, char[] cb) {
+		for (int i = 0; i < ca.length - 1; i++) {
+			if (i < cb.length) {
+				ca[i] += cb[i];
+			}
+			ca[i + 1] += ca[i] / 2;
+			ca[i] %= 2;
+		}
+	}
+
+	private void offset(char[] c, int value) {
+		for (int i = 0; i < c.length; i++) {
+			c[i] += value;
+		}
+	}
+
+	private void reverse(char[] c) {
+		int len = c.length;
+		char tmp;
+		for (int i = 0; i < len / 2; i++) {
+			tmp = c[i];
+			c[i] = c[len - 1 - i];
+			c[len - 1 - i] = tmp;
+		}
+	}
+}
---
+++ Anagrams.java
@@ -0,0 +1,56 @@
+public class Solution {
+    public ArrayList<String> anagrams(String[] strs) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		ArrayList<String> res = new ArrayList<String>();
+		List<Word> anas = new ArrayList<Word>();
+		for (String s : strs) {
+			anas.add(new Word(s));
+		}
+		Collections.sort(anas);
+		int lastIndex = 0;
+		int i = 1;
+		for (; i < anas.size(); i++) {
+			if (anas.get(i).compareTo(anas.get(i - 1)) == 0) {
+				continue;
+			} else {
+				if (i - lastIndex >= 2) {
+					for (int j = lastIndex; j < i; j++) {
+						res.add(anas.get(j).val);
+					}
+				}
+				lastIndex = i;
+			}
+		}
+		if (i - lastIndex >= 2) {
+			for (int j = lastIndex; j < i; j++) {
+				res.add(anas.get(j).val);
+			}
+		}
+		return res;
+	}
+
+	class Word implements Comparable<Word> {
+		String val;
+		int[] features;
+
+		public Word(String v) {
+			val = v;
+			features = new int[26];
+			char[] chars = val.toCharArray();
+			for (char c : chars) {
+				features[c - 'a']++;
+			}
+		}
+
+		@Override
+		public int compareTo(Word w) {
+			for (int i = 0; i < 26; i++) {
+				if (features[i] != w.features[i]) {
+					return features[i] - w.features[i];
+				}
+			}
+			return 0;
+		}
+	}
+}
