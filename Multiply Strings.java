09e971c879dab74f1d9606d41b2f8dcd3d5301de
rffffffff007 <rffffffff007@yahoo.com.cn>
2013-03-27 15:14:48 +0800

    modify answers

--- Multiply Strings.java
+++ Multiply Strings.java
@@ -78,5 +78,63 @@ public class Solution {
 		n[i] = n[j];
 		n[j] = tmp;
 	}
+}
 
+
+public class Solution {
+    public String multiply(String num1, String num2) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        char[] c1 = num1.toCharArray();
+        char[] c2 = num2.toCharArray();
+        reverse(c1);
+        reverse(c2);
+        char[] c3 = new char[c1.length + c2.length + 1];
+        multi(c1, c2, c3);
+        reverse(c3);
+        int index = 0;
+        while(index < c3.length - 1 && c3[index] == '0'){
+            index++;
+        }
+        return new String(c3, index, c3.length - index);
+    }
+    
+    // c3 = c1 * c2
+    private void multi(char[] c1, char[] c2, char[] c3){
+        Arrays.fill(c3, '0');
+        int len1 = c1.length;
+        int len2 = c2.length;
+        for(int i = 0; i < len1; i++){
+            for(int j = 0; j < len2; j++){
+                c3[i + j] += toInt(c1[i]) * toInt(c2[j]);
+                int val = toInt(c3[i + j]);
+                c3[i + j + 1] += val / 10;
+                c3[i + j] = toChar(val % 10);
+            }
+        }
+    }
+
+    private int toInt(char c){
+        return (int)(c - '0');
+    }
+    
+    private char toChar(int i){
+        return (char)(i + '0');
+    }
+    
+    private void reverse(char[] c){
+        reverse(c, 0, c.length - 1);
+    }
+    
+    private void reverse(char[] c, int s, int e){
+        while(s < e){
+            swap(c, s++, e--);
+        }
+    }
+    
+    private void swap(char[] c, int i, int j){
+        char tmp = c[i];
+        c[i] = c[j];
+        c[j] = tmp;
+    }
 }
